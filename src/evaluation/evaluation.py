23afcf106969c3c9e36e3f709b51d7e274b8f6fb
lasse2507a <lran18@student.aau.dk>
2024-12-10 11:55:46 +0100

    initial statistical evaluation

---
+++ src/evaluation/evaluation.py
@@ -0,0 +1,62 @@
+import os
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+sys.path.append("src")
+from utils.load_data import data_loader
+
+
+def feature_distribution(data):
+    # Lists to store calculated statistics
+    means = []
+    medians = []
+    std_devs = []
+    mins = []
+    max_values = []
+    percentiles_25 = []
+    percentiles_50 = []
+    percentiles_75 = []
+
+    for i in range(9):
+        feature_data = data[:, i * 5 : (i + 1) * 5].flatten()
+
+        print("Data shape:", feature_data.shape)
+
+        means.append(np.mean(feature_data))
+        medians.append(np.median(feature_data))
+        std_devs.append(np.std(feature_data))
+        mins.append(np.min(feature_data))
+        max_values.append(np.max(feature_data))
+
+        percentiles_25.append(np.percentile(feature_data, 25))
+        percentiles_50.append(np.percentile(feature_data, 50))
+        percentiles_75.append(np.percentile(feature_data, 75))
+
+        plt.subplot(3, 3, i + 1)
+        plt.hist(feature_data, bins=50, color="skyblue", edgecolor="black", alpha=0.7)
+        plt.title(f"Feature {i+1}")
+        plt.xlabel("Value")
+        plt.ylabel("Frequency")
+
+    print("Means:", means)
+    print("Medians:", medians)
+    print("Standard Deviations:", std_devs)
+    print("Min Values:", mins)
+    print("Max Values:", max_values)
+    print("25th Percentiles:", percentiles_25)
+    print("50th Percentiles (Medians):", percentiles_50)
+    print("75th Percentiles:", percentiles_75)
+
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    data, _ = data_loader(
+        os.path.join("data", "mimic-iii_preprocessed", "pickle_data", "data.pkl"),
+        os.path.join("data", "mimic-iii_preprocessed", "pickle_data", "labels.pkl"),
+    )
+
+    feature_distribution(data)
