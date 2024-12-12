48a703968517a1fb575ef2fd3d050a42c99a370f
Dolmer1 <72791404+Dolmer1@users.noreply.github.com>
2024-12-12 12:20:33 +0100

    small changes

--- src/smartnoise/smartnoise_experiment.py
+++ src/smartnoise/smartnoise_experiment.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 from snsynth import Synthesizer
 from snsynth.pytorch import PytorchDPSynthesizer
-from snsynth.pytorch.nn import DPCTGAN, PATEGAN
+from snsynth.pytorch.nn import DPCTGAN, DPGAN, PATECTGAN, PATEGAN
 
 sys.path.append("src")
 from utils.data_handling import data_loader
@@ -17,16 +17,21 @@ def generate_synthetic_data(data, gans):
         synth.fit(data, preprocessor_eps=0.1)
 
         synthetic_data = synth.sample(8228)
-        synthetic_data.to_csv(os.path.join("data", "synthetic_data", f"smartnoise_{name}"), index=False)
+        synthetic_data.to_csv(os.path.join("data", "synthetic_data", f"smartnoise_{name}.csv"), index=False)
 
 
 if __name__ == "__main__":
-    data, labels = data_loader(
-        os.path.join("data", "mimic-iii_preprocessed", "pickle_data", "train_data.pkl"),
-        os.path.join("data", "mimic-iii_preprocessed", "pickle_data", "train_labels.pkl"),
+    features, labels = data_loader(
+        os.path.join("data", "mimic-iii_preprocessed", "pickle_data", "original_data.pkl"),
+        os.path.join("data", "mimic-iii_preprocessed", "pickle_data", "original_labels.pkl"),
     )
-    data_with_labels = np.concatenate((data, labels), axis=1)
-    df_data = pd.DataFrame(data_with_labels)
+    data = np.concatenate((features, labels), axis=1)
+    data = pd.DataFrame(data)
 
-    gans = {"pategan": PATEGAN(epsilon=1.0), "dpctgan": DPCTGAN()}
-    generate_synthetic_data(df_data, gans)
+    gans = {
+        "dpctgan": DPCTGAN(),
+        "pategan": PATEGAN(epsilon=1.0),
+        "patectgan": PATECTGAN(regularization="dragan"),
+    }
+
+    generate_synthetic_data(data, gans)
